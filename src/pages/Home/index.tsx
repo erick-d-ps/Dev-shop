c2a23fd638cca4dafa6c3607d0c5401bcfc17f0d
deverick <dncellassistec@gmail.com>
2025-03-06 18:24:01 -0300

    criando interface pagina home e cart

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,30 @@
-export function Home(){
-    return(
-        <div>
-            <h1>Pagina Home</h1>
+import { BsCartPlus } from 'react-icons/bs'
+
+
+export function Home() {
+  return (
+    <div>
+      <main className="w-full max-w-7xl px-4 mx-auto">
+        <h1 className="font-bold text-2xl mb-4 mt-10 text-center">
+          Produtos em alta
+        </h1>
+        <div className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5'>
+          <section className="w-full">
+            <img
+            className="w-full rounded-lg max-h-70 mb-2"
+              src="https://lojaibyte.vteximg.com.br/arquivos/ids/427931-1200-1200/47304-1_47304.jpg?v=638496672672300000"
+              alt="Logo do prduto"
+            />
+            <p className="font-medium mt-1 mb-2">Fone JBL</p>
+            <div className='flex gap-3 items-center'>
+              <strong className='text-zinc-700/90'>R$300</strong>
+              <button className='bg-zinc-900 p1 rounded'>
+                <BsCartPlus size={20} color='#FFF'/>
+              </button>
+            </div>
+          </section>
         </div>
-    )
+      </main>
+    </div>
+  );
 }
--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,7 +1,29 @@
 export function Cart() {
     return (
-      <div>
-        <h1>Pagina Carrinho</h1>
+      <div className="w-full max-w-7xl px-2 mx-auto">
+        <h1 className="font-medium text-2xl text-center my-4">Meu carrinho</h1>
+
+        <section className="flex items-center justify-between border-b-2 border-gray-300">
+          <img 
+          src="https://lojaibyte.vteximg.com.br/arquivos/ids/427931-1200-1200/47304-1_47304.jpg?v=638496672672300000" 
+          alt="Logo do produto"
+          className="w-28"
+          />
+          <strong>Preço: R$300,00</strong>
+          <div className="flex gap-3">
+            <button className="bg-slate-600 px-2 rounded text-white font-medium flex  items-center justify-center" >
+              -
+            </button>
+            1
+            <button className="bg-slate-600 px-2 rounded text-white font-medium flex  items-center justify-center" >
+              +
+            </button>
+          </div>
+         <strong className="float-right">
+          SubTotal: R$300,00
+         </strong> 
+        </section>
+        <p className="font-bold mt-4">Total: R$300,00</p>
       </div>
     );
   }
