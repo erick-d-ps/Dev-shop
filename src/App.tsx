b2c9068812d54df8a47c1ed31d13f5d48f27fc8a
deverick <dncellassistec@gmail.com>
2025-03-06 16:56:17 -0300

    inicio do projeto criação de rotas e header

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import { Home } from "./pages/Home";
+import { Cart } from "./pages/cart";
+import { Layout } from './components/layout';
+
+
+const router = createBrowserRouter([
+  {
+    element: <Layout/>,
+    children:[
+      {
+        path:"/",
+        element: <Home/>,
+      },
+      {
+        path:"/cart",
+        element: <Cart/>
+      }
+    ]
+    
+  }
+])
+
+export { router };
---
+++ src/pages/cart/index.tsx
@@ -0,0 +1,7 @@
+export function Cart() {
+    return (
+      <div>
+        <h1>Pagina Carrinho</h1>
+      </div>
+    );
+  }
